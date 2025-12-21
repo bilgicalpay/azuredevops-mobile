1ac8550f904184782fe5a9976bb490b894f9357d
Alpay Bilgiç <bilgicalpay@gmail.com>
2025-12-21 21:46:10 +0300

    .

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "io.rdc.azuredevops"
+    namespace = "com.higgscloud.azuredevops"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "io.rdc.azuredevops"
+        applicationId = "com.higgscloud.azuredevops"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
